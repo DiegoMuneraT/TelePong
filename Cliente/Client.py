74426bd3f01d7c26aacae6145f21f3ea4b8e669c
Diego <damunerat@eafit.edu.co>
2023-10-15 11:28:47 -0500

    Server socket assignment solved

--- Cliente/Client.py
+++ Cliente/Client.py
@@ -18,226 +18,236 @@ Variables de estado
 8: Puntaje B
 
 """
-estado = myp.REQUEST("GET", "STATE")
-print("Estado grande: ", estado)
-
-myp.thread.start()
-
-myp.REQUEST("PLAYER JOIN", "NULL")
-
-port = myp.REQUEST("GET", "PORT")
-
-# Dibujamos la pantalla
-wn = turtle.Screen()
-wn.title("Pong")
-wn.bgcolor("black")
-wn.setup(width=800, height=600)
-wn.tracer(0)
-
-# Puntaje
-score_a = 0
-score_b = 0
-
-# Raqueta A
-paddle_a = turtle.Turtle()
-paddle_a.speed(0)
-paddle_a.shape("square")
-paddle_a.color("red")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
-paddle_a.penup()
-paddle_a.goto(-350, 0)
-
-# Raqueta B
-paddle_b = turtle.Turtle()
-paddle_b.speed(0)
-paddle_b.shape("square")
-paddle_b.color("blue")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
-paddle_b.penup()
-paddle_b.goto(350, 0)
-
-# Bola
-ball = turtle.Turtle()
-ball.speed(0)
-ball.shape("circle")
-ball.color("white")
-ball.penup()
-ball.goto(0, 0)
-ball.dx = 2
-ball.dy = 2
-
-# Puntaje
-pen = turtle.Turtle()
-pen.speed(0)
-pen.shape("square")
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 260)
-pen.write("Cliente A: 0  Cliente B: 0", align="center", font=("Courier", 24, "normal"))
-
-
-# Movimiento de las raquetas
-def paddle_up():
-    if estado[1] == port:
-        y = paddle_a.ycor()
-        y += 20
-        # Se envia el mensaje al servidor con la cabecera (PM: Paddle Move) y la PDU (String con la posición de la raqueta)
-        myp.REQUEST("PADDLE", str(y))
-        paddle_a.sety(y)
-
-    elif estado[2] == port:
-        y = paddle_b.ycor()
-        y += 20
-        # Se envia el mensaje al servidor con la cabecera (PM: Paddle Move) y la PDU (String con la posición de la raqueta)
-        myp.REQUEST("PADDLE", str(y))
-        paddle_b.sety(y)
-
-
-def paddle_down():
-    if estado[1] == port:
-        y = paddle_a.ycor()
-        y -= 20
-        # Se envia el mensaje al servidor con la cabecera (PM: Paddle Move) y la PDU (String con la posición de la raqueta)
-        myp.REQUEST("PADDLE", str(y))
-        paddle_a.sety(y)
-
-    elif estado[2] == port:
-        y = paddle_b.ycor()
-        y -= 20
-        # Se envia el mensaje al servidor con la cabecera (PM: Paddle Move ) y la PDU (String con la posición de la raqueta)
-        myp.REQUEST("PADDLE", str(y))
-        paddle_b.sety(y)
-
-
-# Actualizar la posición de las raquetas
-def update_paddle():
+def main():
     estado = myp.REQUEST("GET", "STATE")
-    print(estado)
+    print("Estado grande: ", estado)
 
-    if estado[1] == port and paddle_b.ycor() != int(estado[4]):
-        paddle_b.sety(int(estado[4]))
+    myp.thread.start()
 
-    if estado[2] == port and paddle_a.ycor() != int(estado[3]):
-        paddle_a.sety(int(estado[3]))
+    myp.REQUEST("PLAYER JOIN", "NULL")
 
+    # Dibujamos la pantalla
+    wn = turtle.Screen()
+    wn.title("Pong")
+    wn.bgcolor("black")
+    wn.setup(width=800, height=600)
+    wn.tracer(0)
 
-def update_score():
-    estado = myp.REQUEST("GET", "STATE")
+    # Puntaje
+    score_a = 0
+    score_b = 0
 
-    if estado[1] == port and score_a != int(estado[7]):
-        score_a = int(estado[7])
-
-    if estado[2] == port and score_b != int(estado[8]):
-        score_b = int(estado[8])
-
-
-def update_ball():
-    # estado = myp.REQUEST("GET", "STATE")
-
-    # # Movimiento de la bola
-    # if estado[1] == port:
-    #     ball.setx(ball.xcor() + ball.dx)
-    #     ball.sety(ball.ycor() + ball.dy)
-    #     myp.REQUEST("BALLX", ball.xcor())
-    #     myp.REQUEST("BALLY", ball.ycor())
-    # else:
-    #     ball.setx(int(estado[5]))
-    #     ball.sety(int(estado[6]))
-
-    ball.setx(ball.xcor() + ball.dx)
-    ball.sety(ball.ycor() + ball.dy)
-
-
-while estado[1] is None or estado[2] == "0000":
-    estado = myp.REQUEST("GET", "STATE")
-    continue
-
-if estado[1] == port:
-    paddle_a.color("blue")
-    paddle_b.color("red")
-else:
+    # Raqueta A
+    paddle_a = turtle.Turtle()
+    paddle_a.speed(0)
+    paddle_a.shape("square")
     paddle_a.color("red")
+    paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+    paddle_a.penup()
+    paddle_a.goto(-350, 0)
+
+    # Raqueta B
+    paddle_b = turtle.Turtle()
+    paddle_b.speed(0)
+    paddle_b.shape("square")
     paddle_b.color("blue")
+    paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+    paddle_b.penup()
+    paddle_b.goto(350, 0)
 
-# Entrada de teclado
-wn.listen()
-# Con cada entrada de teclado se envía un mensaje al servidor actualizando la posición de la raqueta
-wn.onkeypress(paddle_up, "w")
-wn.onkeypress(paddle_down, "s")
+    # Bola
+    ball = turtle.Turtle()
+    ball.speed(0)
+    ball.shape("circle")
+    ball.color("white")
+    ball.penup()
+    ball.goto(0, 0)
+    ball.dx = 2
+    ball.dy = 2
+
+    # Puntaje
+    pen = turtle.Turtle()
+    pen.speed(0)
+    pen.shape("square")
+    pen.color("white")
+    pen.penup()
+    pen.hideturtle()
+    pen.goto(0, 260)
+    pen.write("Cliente A: 0  Cliente B: 0", align="center", font=("Courier", 24, "normal"))
 
 
-# Loop principal del juego
-while score_a < 10 and score_b < 10:
-    wn.update()
+    # Movimiento de las raquetas
+    def paddle_up():
+        if estado[1] == myp.client_server_port:
+            print("oe")
+            y = paddle_a.ycor()
+            y += 20
+            # Se envia el mensaje al servidor con la cabecera (PM: Paddle Move) y la PDU (String con la posición de la raqueta)
+            myp.REQUEST("PADDLE", str(y))
+            paddle_a.sety(y)
 
-    update_paddle()
+        elif estado[2] == myp.client_server_port:
+            print("oe")
+            y = paddle_b.ycor()
+            y += 20
+            # Se envia el mensaje al servidor con la cabecera (PM: Paddle Move) y la PDU (String con la posición de la raqueta)
+            myp.REQUEST("PADDLE", str(y))
+            paddle_b.sety(y)
 
-    update_ball()
 
-    # if estado[1] == port:
-    # Bordes
-    # Arriba y abajo
-    if ball.ycor() > 290:
-        ball.sety(290)
-        # Esto hace que la bola rebote
-        ball.dy *= -1
+    def paddle_down():
+        if estado[1] == myp.client_server_port:
+            print("oe")
+            y = paddle_a.ycor()
+            y -= 20
+            # Se envia el mensaje al servidor con la cabecera (PM: Paddle Move) y la PDU (String con la posición de la raqueta)
+            myp.REQUEST("PADDLE", str(y))
+            paddle_a.sety(y)
 
-        # os.system("afplay bounce.wav&")
+        elif estado[2] == myp.client_server_port:
+            print("oe")
 
-    elif ball.ycor() < -290:
-        ball.sety(-290)
-        # Esto hace que la bola rebote
-        ball.dy *= -1
+            y = paddle_b.ycor()
+            y -= 20
+            # Se envia el mensaje al servidor con la cabecera (PM: Paddle Move ) y la PDU (String con la posición de la raqueta)
+            myp.REQUEST("PADDLE", str(y))
+            paddle_b.sety(y)
 
-    # Izquierda y derecha
-    if ball.xcor() > 350:
-        score_a += 1
 
-        if estado[1] == port:
-            myp.REQUEST("SCORE", str(score_a))
+    # Actualizar la posición de las raquetas
+    def update_paddle():
+        estado = myp.REQUEST("GET", "STATE")
+        #print(estado)
 
-        pen.clear()
-        pen.write(
-            "Cliente A: {}  Cliente B: {}".format(score_a, score_b),
-            align="center",
-            font=("Courier", 24, "normal"),
-        )
-        ball.goto(0, 0)
-        ball.dx *= -1
+        if estado[1] == myp.client_server_port and paddle_b.ycor() != int(estado[4]):
+            paddle_b.sety(int(estado[4]))
 
-    elif ball.xcor() < -350:
-        score_b += 1
+        if estado[2] == myp.client_server_port and paddle_a.ycor() != int(estado[3]):
+            paddle_a.sety(int(estado[3]))
 
-        if estado[2] == port:
-            myp.REQUEST("SCORE", str(score_b))
 
-        pen.clear()
-        pen.write(
-            "Cliente A: {}  Cliente B: {}".format(score_a, score_b),
-            align="center",
-            font=("Courier", 24, "normal"),
-        )
-        ball.goto(0, 0)
-        ball.dx *= -1
+    def update_score():
+        estado = myp.REQUEST("GET", "STATE")
 
-    # Colisiones con las raquetas
-    if (
-        ball.xcor() < -340
-        and ball.ycor() < paddle_a.ycor() + 50
-        and ball.ycor() > paddle_a.ycor() - 50
-    ):
-        ball.dx *= -1
+        if estado[1] == myp.client_server_port and score_a != int(estado[7]):
+            score_a = int(estado[7])
 
-    elif (
-        ball.xcor() > 340
-        and ball.ycor() < paddle_b.ycor() + 50
-        and ball.ycor() > paddle_b.ycor() - 50
-    ):
-        ball.dx *= -1
+        if estado[2] == myp.client_server_port and score_b != int(estado[8]):
+            score_b = int(estado[8])
 
-    time.sleep(0.01)
 
-# Close the socket and wait for the receive thread to finish
-myp.client_socket.close()
-myp.thread.join()
+    def update_ball():
+        # estado = myp.REQUEST("GET", "STATE")
+
+        # # Movimiento de la bola
+        # if estado[1] == port:
+        #     ball.setx(ball.xcor() + ball.dx)
+        #     ball.sety(ball.ycor() + ball.dy)
+        #     myp.REQUEST("BALLX", ball.xcor())
+        #     myp.REQUEST("BALLY", ball.ycor())
+        # else:
+        #     ball.setx(int(estado[5]))
+        #     ball.sety(int(estado[6]))
+
+        ball.setx(ball.xcor() + ball.dx)
+        ball.sety(ball.ycor() + ball.dy)
+
+
+    while estado[1] is None or estado[2] == "0000":
+        estado = myp.REQUEST("GET", "STATE")
+        continue
+
+    if estado[1] == myp.client_server_port:
+        paddle_a.color("blue")
+        paddle_b.color("red")
+    else:
+        paddle_a.color("red")
+        paddle_b.color("blue")
+
+    # Entrada de teclado
+    wn.listen()
+    # Con cada entrada de teclado se envía un mensaje al servidor actualizando la posición de la raqueta
+    wn.onkeypress(paddle_up, "w")
+    wn.onkeypress(paddle_down, "s")
+
+
+    # Loop principal del juego
+    while score_a < 10 and score_b < 10:
+        wn.update()
+
+        if(wn.onkeypress(paddle_up, "w")):
+            print("w")
+
+        update_paddle()
+
+        update_ball()
+
+        # if estado[1] == port:
+        # Bordes
+        # Arriba y abajo
+        if ball.ycor() > 290:
+            ball.sety(290)
+            # Esto hace que la bola rebote
+            ball.dy *= -1
+
+            # os.system("afplay bounce.wav&")
+
+        elif ball.ycor() < -290:
+            ball.sety(-290)
+            # Esto hace que la bola rebote
+            ball.dy *= -1
+
+        # Izquierda y derecha
+        if ball.xcor() > 350:
+            score_a += 1
+
+            if estado[1] == myp.client_server_port:
+                myp.REQUEST("SCORE", str(score_a))
+
+            pen.clear()
+            pen.write(
+                "Cliente A: {}  Cliente B: {}".format(score_a, score_b),
+                align="center",
+                font=("Courier", 24, "normal"),
+            )
+            ball.goto(0, 0)
+            ball.dx *= -1
+
+        elif ball.xcor() < -350:
+            score_b += 1
+
+            if estado[2] == myp.client_server_port:
+                myp.REQUEST("SCORE", str(score_b))
+
+            pen.clear()
+            pen.write(
+                "Cliente A: {}  Cliente B: {}".format(score_a, score_b),
+                align="center",
+                font=("Courier", 24, "normal"),
+            )
+            ball.goto(0, 0)
+            ball.dx *= -1
+
+        # Colisiones con las raquetas
+        if (
+            ball.xcor() < -340
+            and ball.ycor() < paddle_a.ycor() + 50
+            and ball.ycor() > paddle_a.ycor() - 50
+        ):
+            ball.dx *= -1
+
+        elif (
+            ball.xcor() > 340
+            and ball.ycor() < paddle_b.ycor() + 50
+            and ball.ycor() > paddle_b.ycor() - 50
+        ):
+            ball.dx *= -1
+
+        time.sleep(0.01)
+
+    # Close the socket and wait for the receive thread to finish
+    myp.client_socket.close()
+    myp.thread.join()
+
+if __name__ == "__main__":
+    main()
--- Cliente/Protocol/myprotocol.py
+++ Cliente/Protocol/myprotocol.py
@@ -4,7 +4,7 @@ import constants
 
 # Variables de estado
 
-estado = [0, None, None, 0, 0, 0, 0, 0, 0]
+estado = [ 0, None, None, 0, 0, 0, 0, 0, 0]
 
 game_port = 0
 
@@ -14,66 +14,80 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Set the socket to non-blocking
 client_socket.setblocking(0)
 
-client_port = str(client_socket.getsockname()[1])
+print(f"Cliente: {str(client_socket.getsockname()[1])} conectado al servidor: {constants.SERVER_IP} en el puerto: {constants.SERVER_PORT}")
 
-print(
-    f"Cliente: {client_port} conectado al servidor: {constants.SERVER_IP} en el puerto: {constants.SERVER_PORT}"
-)
-
+client_server_port = 0
 
 def get_client_port():
     return str(client_socket.getsockname()[1])
 
-
 def receive_state(client_socket):
-    global estado, game_port
+    global estado, game_port, client_server_port
+    print("receive state")
+    contador = 0
     while True:
         try:
             data, server_address = client_socket.recvfrom(constants.MAX_MSG_LEN)
             rawdata = data.decode().split(":")
+
+            print(f"Recibido: {rawdata}")
+
             if rawdata[0] == "state":
                 estado = rawdata[1].split(",")
+
+                if contador == 0 and estado[0] == "0":
+                    client_server_port = estado[1]
+                    contador+=1
+                elif contador == 0:
+                    client_server_port = estado[2]
+                    contador+=1
+
             elif rawdata[0] == "port":
+                print(f"Recibido puerto del juego: {rawdata[1]}")
                 game_port = int(rawdata[1])
 
-        except socket.error as e:
+            
+        
+        except socket.error:
+            pass
+        
+        except UnboundLocalError:
             pass
 
-
 def send_message(client_socket, message, server_port):
+    print(f"Enviando mensaje: {message} al servidor: {constants.SERVER_IP} en el puerto: {server_port}")
     client_socket.sendto(message.encode(), (constants.SERVER_IP, server_port))
-
 
 thread = threading.Thread(target=receive_state, args=(client_socket,))
 
-
 def REQUEST(header, value):
-    if header == "GET" and value == "PORT":
+
+    if header == 'GET' and value == 'PORT':
         return get_client_port()
 
-    elif header == "GET" and value == "STATE":
+    elif header == 'GET' and value == 'STATE':
         return estado
-
-    elif header == "PADDLE":
-        send_message(client_socket, "PM" + str(value), game_port)
-        return "PADDLE MOVE OK"
-
-    elif header == "BALLX":
-        send_message(client_socket, "BX" + str(value), game_port)
-        return "BALL MOVE OK"
-
-    elif header == "BALLY":
-        send_message(client_socket, "BY" + str(value), game_port)
-        return "BALL MOVE OK"
-
-    elif header == "PLAYER JOIN":
-        send_message(client_socket, "PLJ", constants.SERVER_PORT)
-        return "PLAYER JOINED"
-
-    elif header == "SCORE":
-        send_message(client_socket, "SC" + str(value), game_port)
-
-
+    
+    elif header == 'PADDLE':
+        send_message(client_socket, 'PM'+str(value), game_port)
+        return 'PADDLE MOVE OK'
+    
+    elif header == 'BALLX':
+        send_message(client_socket, 'BX'+str(value), game_port)
+        return 'BALL MOVE OK'
+    
+    elif header == 'BALLY':
+        send_message(client_socket, 'BY'+str(value), game_port)
+        return 'BALL MOVE OK'
+    
+    elif header == 'PLAYER JOIN':
+        print("oe")
+        send_message(client_socket, 'PLJ', constants.SERVER_PORT)
+        return 'PLAYER JOINED'
+    
+    elif header == 'SCORE':
+        send_message(client_socket, 'SC'+str(value), game_port)
+        
 """
 
 REQUEST es un método que se encarga de enviar una solicitud al servidor.
